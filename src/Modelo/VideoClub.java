0010567b51c805c809ecf6f843ff851574f86528
Radianx <>
2018-09-17 17:39:56 -0300

    My first git commit

---
+++ src/Modelo/VideoClub.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Family
+ */
+public class VideoClub {
+    private List<Encargado>listaEncargados = new ArrayList<>();
+    private List<Socio>listaSocios = new ArrayList<>();
+    private List<Prestamo>listaPrestamos = new ArrayList<>();
+    private List<Reserva>listaReserva = new ArrayList<>();
+    private List<Pelicula>listaPeliculas = new ArrayList<>();
+    private List<Genero>listaGenero = new ArrayList<>();
+    
+    
+        
+}
